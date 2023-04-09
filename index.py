0919d5de8162ce41f0c43be0257ef79cc75880f0
Eniola Hannah <heniola395@gmail.com>
2023-04-09 09:59:02 +0100

    I created the program for natural number ppassword

--- index.py
+++ index.py
@@ -12,7 +12,25 @@ user = input("""
     - """)
 
 if user == "1":
-    pass
+    user1 = input("""
+        How many digit password do you want:
+            - 4
+            - 6
+            - 8
+            - 10
+                  - """)
+    if user1 == "4":
+        a = int(numpy.random.random()*10000)
+        print("here you go -- " + str(a))
+    elif user1 == "6":
+        b = int(numpy.random.random()*1000000)
+        print("here you go -- " + str(b))
+    elif user1 == "8":
+        c = int(numpy.random.random()*100000000)
+        print("here you go -- " + str(c))
+    elif user1 == "10":
+        d = int(numpy.random.random()*10000000000)
+        print("here you go -- " + str(d))
 elif user == "2":
     pass
 elif user == "3":

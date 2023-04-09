3300ddea7cdccb778daca21a57c9cc19b8e21525
Eniola Hannah <heniola395@gmail.com>
2023-04-09 09:43:54 +0100

    I created options for the user for  the password generator

---
+++ index.py
@@ -0,0 +1,9 @@
+import numpy
+
+user = input("""
+    Welcome, So here I have a program that geerate random password:
+    here are my options;
+        1. All numbers
+        2. All letters
+        3. Mixture
+    - """)
